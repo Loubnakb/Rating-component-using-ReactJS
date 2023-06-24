d8ac0730dc7ad8c0f301560a9cbeea5da039e3bf
Loubnakb <loubna.kouidribenfredj@gmail.com>
2023-06-24 15:44:11 +0100

    Rating component using ReactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Rating from "./components/Rating";
+import Card from "./components/Card";
+import ThankYou from "./components/ThankYou";
+import { useState } from "react";
 
 function App() {
+  const[showThankYouPage, setShowThankYouPage] = useState(false);
+  const[rating, setRating] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {showThankYouPage ? (
+      <ThankYou rating={rating}/>
+      ) : (
+      <Rating rating={rating} 
+      setRating={setRating} 
+      setShowThankYouPage={setShowThankYouPage} />
+      )};
+    </>
   );
 }
 
---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+import "./ThankYou.css";
+import tY from "../media/illustration-thank-you.svg";
+
+const ThankYou = ({rating}) => {
+  return (
+    <Card>
+      <div className="thank-you-img-container">
+        <img src={tY} alt="" />
+      </div>
+      <div className="selected">
+        <p>You selected {rating} out of 5</p>
+      </div>
+      <h2 className="title">Thank You</h2>
+      <p className="text">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don't hesitate to get in touch
+      </p>
+    </Card>
+  );
+};
+
+export default ThankYou;
